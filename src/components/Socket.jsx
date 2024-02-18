3f16faf389b41ab0aa283ca20e106263a9672a4e
karim sherif <119266066+kiwi107@users.noreply.github.com>
2024-02-18 21:32:31 +0200

    Update Socket.jsx

--- src/components/Socket.jsx
+++ src/components/Socket.jsx
@@ -1,9 +1,13 @@
 import { io } from 'socket.io-client';
-
-
+let jwtRegex = /JWT=([^;]+)/;
+let jwtMatch = document.cookie.match(jwtRegex);
 const Socket = new io(process.env.REACT_APP_SERVER_URL, {
     autoConnect: false,
     withCredentials: true,
+    transports: ['websocket'],
+    auth: {
+        token: jwtMatch[1] || ''
+    }
 });
 
 
