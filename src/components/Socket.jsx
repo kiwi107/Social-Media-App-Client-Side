9ffc0218f17b08771d483302f0b6166732939f29
karim sherif <119266066+kiwi107@users.noreply.github.com>
2024-02-18 20:58:47 +0200

    Update Socket.jsx

--- src/components/Socket.jsx
+++ src/components/Socket.jsx
@@ -4,7 +4,6 @@ import { io } from 'socket.io-client';
 const Socket = new io(process.env.REACT_APP_SERVER_URL, {
     autoConnect: false,
     withCredentials: true,
-    transports: ['websocket']
 });
 
 
